f9b5529374ff49976920621354e49072bc02cd19
DavidEsdrs <carpina1983@gmail.com>
2023-05-05 18:45:39 -0300

    feat: Use auth middleware

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import { buildCreateUser } from "./useCases/users/create/CreateUser.builder";
 import { buildLoginUser } from "./useCases/users/login/LoginUser.builder";
 import { buildCreatePost } from "./useCases/posts/create/CreatePost.builder";
 import { createPostMiddlewares } from "./useCases/posts/create/CreatePost.middleware";
+import { ensureAuth } from "./middlewares/ensureAuth";
 
 const router = Router();
 
@@ -11,6 +12,8 @@ router.get("/", (req, res) => res.json("running"));
 router.post("/users", (req, res) => buildCreateUser().handle(req, res));
 
 router.post("/users/login", (req, res) => buildLoginUser().handle(req, res));
+
+router.use(ensureAuth);
 
 router.post("/posts", ...createPostMiddlewares, (req, res) => buildCreatePost().handle(req, res));
 
