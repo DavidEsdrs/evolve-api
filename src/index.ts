8889a2b3523f9451a1987a0cc491eb323edfe74e
DavidEsdrs <carpina1983@gmail.com>
2023-05-07 18:29:34 -0300

    feat: Install and implements cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,23 @@
 import express from "express";
 import "express-async-errors";
 import { router } from "./router";
+import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+    console.log({origin})
+    if(origin === process.env.FRONT_END_URL) {
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+    }
+    next();
+});
+app.use(cors({
+    origin: process.env.FRONT_END_URL,
+    credentials: true
+}));
 app.use(express.json());
 app.use("/api", router);
 app.use(errorHandler);
