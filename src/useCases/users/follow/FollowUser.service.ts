eff61643e6943f99da5a91c4a7c9d4772f8418ea
DavidEsdrs <carpina1983@gmail.com>
2023-05-07 18:38:19 -0300

    feat: add validation to prevent following the same user more than once and following yourself~

--- src/useCases/users/follow/FollowUser.service.ts
+++ src/useCases/users/follow/FollowUser.service.ts
@@ -7,6 +7,18 @@ export class FollowUserService {
     ) {}
 
     async execute({ follower_id, following_id }: IFollowUserDTO) {
+        if(follower_id === following_id) {
+            throw new Error("Can't follow yourself!");
+        }
+        
+        const userAlreadyFollowedUser = await this.prisma.followerMap.findFirst({
+            where: { AND: [ { followerId: follower_id }, { followingId: following_id } ] }
+        });
+
+        if(userAlreadyFollowedUser) {
+            throw new Error("Can't follow the same user more than once!");
+        }
+
         await this.prisma.followerMap.create({
             data: {
                 followerId: follower_id,
