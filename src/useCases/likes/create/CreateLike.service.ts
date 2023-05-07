a1c5da0bc2cef91fecd525f2d48daa807d17c399
DavidEsdrs <carpina1983@gmail.com>
2023-05-07 18:30:12 -0300

    feat: Check if the user already liked the post before saving it

--- src/useCases/likes/create/CreateLike.service.ts
+++ src/useCases/likes/create/CreateLike.service.ts
@@ -7,6 +7,14 @@ export class CreateLikeService {
     ) {}
 
     async execute({ post_id, user_id }: ICreateLikeDTO) {
+        const userAlreadyLikedPost = await this.prisma.like.findFirst({ 
+            where: { AND: [ { postId: post_id }, { userId: user_id } ] }
+        });
+
+        if(userAlreadyLikedPost) {
+            throw new Error("Can't like the same post more than once!");
+        }
+
         await this.prisma.like.create({
             data: {
                 postId: post_id,
